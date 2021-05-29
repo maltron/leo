354b0f5c72bee1d9161ac9199e07cc33d1b63594
Mauricio "Maltron" Leal <maltron@gmail.com>
2021-05-28 21:12:04 -0300

    First commit

--- Main.java
+++ Main.java
@@ -5,15 +5,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Addicionando 10.000 elementos");
         HashSet<Integer> myCollection = new HashSet<Integer>();
+        long inicio = System.currentTimeMillis();
         for(int i=0; i < 10000; i++) {
             myCollection.add(i);
         }
-        System.out.println("Pronto");
+        long fim = System.currentTimeMillis();
+        System.out.println("Pronto: "+(fim - inicio)+"ms");
 
         System.out.println("Removendo 5.000 elementos");
+        inicio = System.currentTimeMillis();
         for(int i=0; i < 5000; i++) {
             myCollection.remove(i);
         }
-        System.out.println("Pronto");
+        fim = System.currentTimeMillis();
+        System.out.println("Pronto: "+(fim - inicio)+"ms");
     }
 }
